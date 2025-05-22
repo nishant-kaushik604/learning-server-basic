533ce3d436a7f1a04ea8d4c8d9ab619e45cfb718
Nishant Kaushik <>
2025-05-22 18:44:15 +0530

    tcp_echo server

---
+++ common/network/tcp_echo_server.cpp
@@ -0,0 +1,73 @@
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class TcpSession : public std::enable_shared_from_this<TcpSession>{
+public:
+    TcpSession(tcp::socket socket) : socket_(std::move(socket)){}
+    
+    void start(){
+        do_read();
+    }
+
+private:
+    void do_read(){
+        auto self(shared_from_this());
+        socket_.async_read_some(
+            boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length){
+                if(!ec){
+                    do_write(length);
+                }
+            }
+        );
+    }
+
+    void do_write(std::size_t length){
+        auto self(shared_from_this());
+        boost::asio::async_write(
+            socket_,
+            boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length){
+                if (!ec)
+                {
+                    do_read();
+                }
+                
+            }
+        );
+    }
+
+    tcp::socket socket_;
+    enum {max_length = 1024};
+    char data_[max_length];
+};
+
+class TcpServer{
+public:
+    TcpServer(boost::asio::io_context& io_context, short port) : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)){
+        do_accept();
+    }
+
+private:
+    void do_accept(){
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket){
+                if (!ec)
+                {
+                    std::make_shared<TcpSession>(std::move(socket))->start();
+                }
+                do_accept();
+            }
+        );
+    }
+
+    tcp::acceptor acceptor_;
+};
+
+int main(){
+    boost::asio::io_context io_context;
+    TcpServer server(io_context, 12345);
+    io_context.run();
+    return 0;
+}
